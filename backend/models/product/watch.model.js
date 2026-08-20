7581a28fb54379de94a992394b9bf43b46d2460c
sumitbolla7 <156924111+sumitbolla7@users.noreply.github.com>
2026-08-21 02:15:21 +0530

    Update watch.model.js schema to support Mixed subCategory

--- backend/models/product/watch.model.js
+++ backend/models/product/watch.model.js
@@ -22,8 +22,8 @@ const watchSchema = new mongoose.Schema({
     immutable: true,
   },
   subCategory: {
-    type: String,
-    trim: true,
+    type: mongoose.Schema.Types.Mixed,
+    default: ''
   },
   gender: {
     type: String,
